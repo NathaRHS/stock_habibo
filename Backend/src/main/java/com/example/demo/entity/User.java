7030e4105a135b96480873445263850f8347f3a8
NathaRHS <sylvanonath@gmail.com>
2026-08-13 22:53:35 +0300

    gestion de roles sur les pages

--- Backend/src/main/java/com/example/demo/entity/User.java
+++ Backend/src/main/java/com/example/demo/entity/User.java
@@ -16,7 +16,7 @@ public class User {
 
     @Column(name = "email", nullable = false, unique = true)
     private String email;
-
+    
     @Column(name = "password_hash", nullable = false)
     private String passwordHash;
 
